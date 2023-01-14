ad326d4f6371d51d3c6fc1aef2b9a9cc06fd7cd3
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-14 15:33:06 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/LazyDeveloperr/MdiskSearchBotV3.git /MdiskSearchBotV3
+cd /MdiskSearchBotV3
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
