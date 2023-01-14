cf74931a2e064c1123a64df04b6b3ce8e00e1b08
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-14 15:38:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/LazyDeveloperr/MdiskSearchBotV2.git /MdiskSearchBotV2
 cd /MdiskSearchBotV2
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
